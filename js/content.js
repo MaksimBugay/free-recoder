27efbf605731fbb9f0951461244a76840163da7a
maksim.bugai <maksim.bugai@multiloginapp.com>
2024-07-10 08:19:11 +0300

    main changes

--- js/content.js
+++ js/content.js
@@ -1,7 +1,43 @@
+const videoMimeTypes = [
+    'video/mp4; codecs="avc1.42E01E, mp4a.40.2"',
+    'video/webm; codecs="vp8,vorbis"',
+    'video/mp4; codecs="avc1.42E01E"',
+    'video/mp4;codecs="avc1.42001f,opus"',
+    'video/ogg; codecs="theora,vorbis"',
+    'video/x-matroska;codecs="avc1,opus"'
+];
+
+for (const mimeType of videoMimeTypes) {
+    if (MediaSource.isTypeSupported(mimeType)) {
+        console.log(`Browser supports ${mimeType} for MediaSource`);
+    } else {
+        console.log(`Browser does not support ${mimeType} for MediaSource`);
+    }
+
+    if (MediaRecorder.isTypeSupported(mimeType)) {
+        console.log(`Browser supports ${mimeType} for MediaRecorder`);
+    } else {
+        console.log(`Browser does not support ${mimeType} for MediaRecorder`);
+    }
+}
+
+const mimeType = videoMimeTypes[0];
+
+const videoPlayer = document.createElement('video');
+videoPlayer.id = 'vPlayer';
+videoPlayer.style.position = 'absolute';
+videoPlayer.style.width = "500px";
+videoPlayer.style.height = "300px";
+videoPlayer.style.top = '20px';
+videoPlayer.style.zIndex = '99999';
+document.body.appendChild(videoPlayer);
+videoPlayer.controls = true;
+
+let mediaSource = null;
+let sourceBuffer = null;
+
 let mediaRecorder;
-let stream;
-const chunkInterval = 10000; // 10 seconds
-let recording = false;
+const chunkInterval = 5000; // 5 seconds
 const chunks = [];
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -16,73 +52,79 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         sendResponse(result);
         return true; // Keeps the message channel open for asynchronous sendResponse
     }
-    if (message.action === 'getNextChunkUrl') {
-        URL.revokeObjectURL(message.finishedVideoUrl);
-        if (chunks.length > 0) {
-            sendResponse({url: chunks.shift()});
-        } else {
-            sendResponse({url: null});
-        }
-    }
 });
 
-async function startRecording(sendResponse) {
+async function startRecording() {
     try {
-        stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
-        recording = true;
-        recordChunk();
+        const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
+        mediaRecorder = new MediaRecorder(stream, {mimeType: mimeType});
+
+        mediaRecorder.ondataavailable = event => {
+            if (event.data.size > 0) {
+                const blob = event.data;
+                console.log('Blob type:', blob.type); // Check Blob type (should be 'video/webm' or 'video/mp4')
+
+                chunks.push(blob);
+                console.log(`${chunks.length} chunks were recorded`);
+            }
+        };
+
+        mediaRecorder.start(chunkInterval);
         return {status: 0, message: 'recording started'};
     } catch (err) {
         return {status: -1, message: `Cannot start, error accessing media devices: ${err}`};
     }
 }
 
-function recordChunk() {
-    if (!recording) return;
 
-    mediaRecorder = new MediaRecorder(stream, {mimeType: 'video/webm'});
-
-    mediaRecorder.ondataavailable = event => {
-        if (event.data.size > 0) {
-            const blob = new Blob([event.data], {type: 'video/webm'});
-            const url = URL.createObjectURL(blob);
-            const filename = `recording_chunk_${Date.now()}.webm`;
-
-            /*chrome.runtime.sendMessage({action: 'saveChunk', url, filename}, (response) => {
-                console.log(response.status);
-                URL.revokeObjectURL(url);
-            });*/
-            chunks.push(url);
-            console.log(`${chunks.length} chunks were recorded`);
-        }
-    };
-
-    mediaRecorder.onstop = () => {
-        if (recording) {
-            setTimeout(recordChunk, 100); // Slight delay before starting the next chunk
-        } else {
-            stream.getTracks().forEach(track => track.stop());
-        }
-    };
-
-    mediaRecorder.start();
-    setTimeout(() => {
-        mediaRecorder.stop();
-    }, chunkInterval);
+function playRecording() {
+    mediaSource = new MediaSource();
+    videoPlayer.src = URL.createObjectURL(mediaSource);
+    mediaSource.addEventListener('sourceopen', () => {
+        sourceBuffer = mediaSource.addSourceBuffer(mimeType);
+        sourceBuffer.mode = 'sequence';
+        appendNextChunk();
+    });
+    videoPlayer.play();
 }
 
+function appendNextChunk() {
+    console.log("Append next chunk request");
+    if (chunks.length === 0) {
+        mediaSource.endOfStream();
+        return;
+    }
+
+    if (!sourceBuffer.updating) {
+        const chunk = chunks.shift();
+        chunk.arrayBuffer().then(buffer => {
+            sourceBuffer.appendBuffer(buffer);
+            console.log("new chunk was appended");
+        }).catch(error => {
+            console.error('Failed to convert blob to arrayBuffer: ', error);
+        });
+    } else {
+        sourceBuffer.addEventListener('updateend', appendNextChunk, {once: true});
+    }
+}
+
+
 function stopRecording() {
-    recording = false;
     try {
         if (mediaRecorder && mediaRecorder.state !== 'inactive') {
             mediaRecorder.stop();
         }
         if (chunks.length > 0) {
-            const url = chunks.shift();
-            chrome.runtime.sendMessage({action: 'playChunk', url});
+            const firstChunk = chunks.shift();
+            console.log('First chunk blob type:', firstChunk.type);
+            playRecording();
         }
         return {status: 0, message: 'recording stopped'};
     } catch (err) {
         return {status: -1, message: `Cannot stop, error accessing media devices: ${err}`};
     }
+}
+
+function delay(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,3 @@
-const videoPlayer = document.getElementById('videoPlayer');
 document.getElementById('startBtn').addEventListener('click', () => {
     chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, {action: 'startRecording'}, (response) => {
@@ -21,41 +20,4 @@ document.getElementById('stopBtn').addEventListener('click', () => {
             }
         });
     });
-});
-
-chrome.runtime.onMessage.addListener((message) => {
-    if (message.action === 'playChunk') {
-        playVideo(message.url);
-        return true; // Keeps the message channel open for asynchronous sendResponse
-    }
-});
-
-function playNextChunk() {
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, {
-            action: 'getNextChunkUrl',
-            finishedVideoUrl: videoPlayer.src
-        }, (response) => {
-            console.log("Get next chunk response");
-            console.log(response);
-            if (response.url) {
-                try {
-                    playVideo(response.url);
-                } catch (err) {
-                    console.error("Failed to play video attempt: " + err);
-                }
-            }
-        });
-    });
-}
-
-function playVideo(blobUrl) {
-    if (blobUrl) {
-        videoPlayer.src = blobUrl;
-        videoPlayer.play();
-    }
-}
-
-videoPlayer.addEventListener('ended', function () {
-    playNextChunk();
 });
