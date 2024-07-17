8901f66125be89d2505f68cc535a5aa9a01fcd2f
maksim.bugai <maksim.bugai@multiloginapp.com>
2024-07-17 12:33:22 +0300

    recorder as simple html page not extension: play chunks with dynamic media source

--- js/simple-player.js
+++ js/simple-player.js
@@ -78,6 +78,7 @@ video.addEventListener('timeupdate', function () {
         if ((currentTime >= endOfBuffered - 1) && streamWasStopped) {
             console.log('All buffered data has been played.');
             delay(3000).then(() => {
+                streamWasStopped = false;
                 cleanMediaSourceBufferAndStopPlaying(video, mediaSource, queue);
             });
         }
